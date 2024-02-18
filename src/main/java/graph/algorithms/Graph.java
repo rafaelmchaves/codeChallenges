da6cf9ee9267e436c5a63fd4b8fc4e5145567243
Rafael Marques Chaves <rafaelmchaves@gmail.com>
2024-02-18 10:03:03 -0300

    Create the DFS search method
    Created in a recursive and iterate way

--- src/main/java/graph/algorithms/Graph.java
+++ src/main/java/graph/algorithms/Graph.java
@@ -25,16 +25,72 @@ public class Graph {
         adjNodes.get(nodeB).add(nodeA);
     }
 
+    boolean searchDFS(String root, String destination) {
+
+        final var nodeDestination = new Node(destination);
+        Set<Node> visited = new LinkedHashSet<>();
+        Stack<Node> stack = new Stack<>();
+        stack.add(new Node(root));
+
+        while (!stack.isEmpty()) {
+            final var node = stack.pop();
+            if (node.equals(nodeDestination)) {
+                return true;
+            }
+            if (!visited.contains(node)) {
+                visited.add(node);
+                for (Node n: adjNodes.get(node)) {
+                    stack.push(n);
+//                    searchDFS(n.getLabel(), destination);
+                }
+            }
+        }
+
+        return false;
+    }
+
+    boolean searchDFSRecursive(String root, String destination, Set<Node> visited) {
+
+        final var nodeDestination = new Node(destination);
+
+        final var node = new Node(root);
+
+        visited.add(node);
+        if (node.equals(nodeDestination)) {
+            return true;
+        }
+
+        for (Node n: adjNodes.get(node)) {
+            if (!visited.contains(n)) {
+                if (searchDFSRecursive(n.getLabel(), destination, visited)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
     public static void main(String[] args) {
        final var graph = new Graph();
        graph.addNode("a");
        graph.addNode("b");
        graph.addNode("c");
        graph.addNode("d");
+       graph.addNode("e");
+       graph.addNode("f");
 
        graph.addBiDirectionalEdge("a", "b");
-       graph.addOneDirectionalEdge("a", "c");
        graph.addBiDirectionalEdge("b", "c");
        graph.addOneDirectionalEdge("c", "d");
+       graph.addOneDirectionalEdge("c", "a");
+       graph.addOneDirectionalEdge("c", "e");
+       graph.addOneDirectionalEdge("a", "f");
+       graph.addOneDirectionalEdge("f", "e");
+
+//        System.out.println("Destination node has found:" + graph.searchDFS("a", "e"));
+        System.out.println("Destination node has found:" + graph.searchDFSRecursive("a", "e", new LinkedHashSet<>()));
+        System.out.println("Destination node has found:" + graph.searchDFSRecursive("a", "d", new LinkedHashSet<>()));
+        System.out.println("Destination node has found:" + graph.searchDFSRecursive("d", "a", new LinkedHashSet<>()));
     }
 }
